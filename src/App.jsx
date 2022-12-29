2d9ac393a99d74967d188e24456c1695a47a0230
aimunNaharr <aimunnaharr@gmail.com>
2022-12-29 20:29:13 +0600

    theme and routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './scenes/home/Home'
+import ItemDetail from './scenes/itemDetail/ItemDetail'
+import Checkout from './scenes/checkout/Checkout'
+import Confirmation from './scenes/checkout/Confirmation'
+import Navbar from './scenes/global/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const ScrollToTop=()=>{
+    const {pathname}=useLocation()
+    useEffect(()=>{
+      window.scrollTo(0, 0)
+    },[ pathname])
+    return null
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main className="app">
+      <BrowserRouter>
+      <Navbar/>
+      <ScrollToTop/>
+      <Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='item/:id' element={<ItemDetail/>}/>
+<Route path='/checkout' element={<Checkout/>}/>
+<Route path='/checkout/success' element={<Confirmation/>}/>
+      </Routes>
+      </BrowserRouter>
+    </main>
   )
 }
 
