6eb10b350ad53a55a907690042e6d0c383011875
aimunNaharr <aimunnaharr@gmail.com>
2023-03-05 12:08:05 +0600

    add product feature added

--- src/scenes/dashboard/AddProduct.jsx
+++ src/scenes/dashboard/AddProduct.jsx
@@ -1,50 +1,95 @@
 import { Box, Button, IconButton, InputLabel, MenuItem, Select, Stack, TextField, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
-import ImageIcon from '@mui/icons-material/Image';
+import ImageIcon from "@mui/icons-material/Image";
 import { shades } from "../../theme";
+import Filebase from "react-file-base64";
+import { usePostProductMutation } from "../../state/productSlice";
 
 const AddProduct = () => {
 	const [category, setCategory] = React.useState("");
-	const [img, setImg] = React.useState(null);
+	const [image, setImg] = React.useState(null);
+	const [name, setName] = React.useState("");
+	const [price, setPrice] = React.useState("");
+	const [description, setDescription] = React.useState("");
+	const [postProduct, { isError, isLoading, isSuccess, data, status }] = usePostProductMutation();
 	const isNonMobile = useMediaQuery("(min-width:600px)");
-    const imageRef=React.useRef()
-    console.log(imageRef)
+	const imageRef = React.useRef();
 	const handleChange = (e) => {
-       
-        setCategory(e.target.value)
-    };
-    const onImageChange=(e)=>{
-       if(e.target.files[0]){
-        setImg(e.target.files[0])
-       }
-    }
+		setCategory(e.target.value);
+	};
+	const onImageChange = (e) => {
+		if (e.target.files[0]) {
+			const file = e.target.files[0];
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onloadend = () => {
+				setImg(reader.result);
+			};
+		}
+	};
+	const handleSubmit = () => {
+		if ((name, category, description, price, image)) {
+			postProduct({name, category, description, price, image});
+		}
+	};
+	if(isSuccess){
+		return <p>Product added successfully</p>
+	}
+	
 	return (
 		<Box p={4} width="100%">
 			<Typography variant="h5">Add a product</Typography>
 			<Box width={isNonMobile ? "80%" : "100%"} sx={{ my: 4 }}>
 				<Stack gap={2}>
 					<Stack flexDirection={isNonMobile ? "row" : "column"} gap={2}>
-						<TextField type="text" required label="Product Name" fullWidth />
-						<TextField type="text" required label="Description" fullWidth />
+						<TextField type="text" required label="Product Name" fullWidth onChange={(e) => setName(e.target.value)} />
+						<TextField type="text" required label="Description" fullWidth onChange={(e) => setDescription(e.target.value)} />
 					</Stack>
 					<Stack flexDirection={isNonMobile ? "row" : "column"} gap={2}>
-						<TextField select value={category}  label="Categories" onChange={handleChange} fullWidth>
-							<MenuItem value={'newArrivals'}>New Arrivals</MenuItem>
-							<MenuItem value={'topRated'}>Top Rated</MenuItem>
-							<MenuItem value={'bestSellers'}>Best Sellers</MenuItem>
+						<TextField select value={category} label="Categories" onChange={handleChange} fullWidth required>
+							<MenuItem value={"newArrivals"}>New Arrivals</MenuItem>
+							<MenuItem value={"topRated"}>Top Rated</MenuItem>
+							<MenuItem value={"bestSellers"}>Best Sellers</MenuItem>
 						</TextField>
-						<TextField type="text" required label="Price" fullWidth />
+						<TextField type="text" required label="Price" fullWidth onChange={(e) => setPrice(e.target.value)} />
 					</Stack>
-                    <Stack>
-                        {
-                            img ?  <div style={{positon: 'relative'}}>
-                             <button onClick={()=>setImg(null)} style={{position: 'absolute', backgroundColor: 'whitesmoke', border: 'none', cursor: 'pointer'}}>X</button>
-                                <img height='200px' width='200px' src={URL.createObjectURL(img)} alt='previewImg'/>
-                            </div>: <div onClick={() => imageRef.current.click()} style={{border: '1px dashed', height: '100px' , width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', cursor: 'pointer'}}> <ImageIcon sx={{ fontSize: 30 }}/><Typography variant="h4">Upload image</Typography></div>
-                        }
-                        <input type='file' style={{display: 'none'}} ref={imageRef} onChange={onImageChange}/>
-                    </Stack>
-                <Button fullwidth sx={{backgroundColor: shades.primary[400], color: '#fff' ,"&:hover":{ backgroundColor: shades.primary[500]}}}>Add</Button>
+					<Stack>
+						{image ? (
+							<div style={{ positon: "relative" }}>
+								<button onClick={() => setImg(null)} style={{ position: "absolute", backgroundColor: "whitesmoke", border: "none", cursor: "pointer" }}>
+									X
+								</button>
+								<img width="200px" src={image} alt="previewImg" />
+							</div>
+						) : (
+							<div
+								onClick={() => imageRef.current.click()}
+								style={{
+									border: "1px dashed",
+									height: "100px",
+									width: "100%",
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+									cursor: "pointer",
+								}}
+							>
+								{" "}
+								<ImageIcon sx={{ fontSize: 30 }} />
+								<Typography variant="h4">Upload image</Typography>
+							</div>
+						)}
+						<input type="file" style={{ display: "none" }} ref={imageRef} onChange={onImageChange} />
+					</Stack>
+					<Button
+						type="submit"
+						onClick={handleSubmit}
+						fullwidth
+						disabled={isLoading}
+						sx={{ backgroundColor: shades.primary[400], color: "#fff", "&:hover": { backgroundColor: shades.primary[500] } }}
+					>
+						{isLoading ? 'Adding': 'Add'}
+					</Button>
 				</Stack>
 			</Box>
 		</Box>
